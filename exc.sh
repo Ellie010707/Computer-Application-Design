0bbe2b5a9f79a8668dd9d9650d6e3747c851f248
Ellie <doris0707@naver.com>
2022-12-21 16:08:07 +0900

    fin

--- exc.sh
+++ exc.sh
@@ -1,2 +1,16 @@
+docker-compose up -d --build
+
 pip install docker
+pip docker-compose==1.29.2
+pip dockerpty==0.4.1
+pip psutil==5.9.4
+pip platformdirs==2.5.1
+
+#execute health check module
 python healthcheck/health_check.py
+
+
+pip install -U pip==20.3
+pip install --use-deprecated=legacy-resolver -r docker==4.4.1
+#execute monitoring&dynamic allocation module
+python3 monitoring/monitoring.py
--- start.sh
+++
@@ -1,3 +0,0 @@
-docker-compose up -d --build
-python3 monitoring/monitoring.py
-python3 healthcheck/healthcheck.py
